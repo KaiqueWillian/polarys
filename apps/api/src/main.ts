7fcfe688f1ecea8e0d4e1a7b8ea7d554ec5b223d
Kaique Will <devkaique23@gmail.com>
2025-10-18 20:33:17 -0300

    chore: estrutura base do monorepo (web + api)

---
+++ apps/api/src/main.ts
@@ -0,0 +1,20 @@
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from './app.module';
+
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+//   await app.listen(process.env.PORT ?? 3333);
+// }
+// void bootstrap();
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const port = process.env.PORT || 4000;
+  await app.listen(port);
+
+  console.log(`🚀 API rodando em: http://localhost:${port}`);
+}
+void bootstrap();
